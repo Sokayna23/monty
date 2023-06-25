eb2de136250a30d01a8e933b790edc7e10353ced
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-25 18:38:42 -0400

    Task6: fix: error message form

--- op_functions2.c
+++ op_functions2.c
@@ -55,7 +55,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !*stack || !((*stack)->next))
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		data.status = EXIT_FAILURE;
 		return;
 	}
